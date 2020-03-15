2e65f23c2c1190f2b1b512709cfa39a2412b6e77
Erik Chan <erik.chan@gmail.com>
2020-03-15 19:04:34 +0800

    Update posts_controller.rb

--- app/controllers/comfy/blog/posts_controller.rb
+++ app/controllers/comfy/blog/posts_controller.rb
@@ -15,7 +15,32 @@ class Comfy::Blog::PostsController < Comfy::Cms::BaseController
     scope = scope.for_category(params[:category]) if params[:category]
     scope = scope.order(:published_at).reverse_order
 
-    @blog_posts = comfy_paginate(scope, per_page: ComfyBlog.config.posts_per_page)
+    @blog_posts = Rails.cache.fetch('all_blog_posts', expires_in: 1.day) do  
+      @blog_posts = comfy_paginate(scope, per_page: ComfyBlog.config.posts_per_page)
+    end
+    
+    @recent_posts = Rails.cache.fetch('recent_blog_posts', expires_in: 1.day) do 
+      @recent_posts = @cms_site.blog_posts.published.order(:published_at).reverse_order.limit(10)
+    end
+  
+    @top_posts = Rails.cache.fetch('top_blog_posts', expires_in: 10.days) do 
+      @top_posts ||= []
+      boo = Ahoy::Event.group(:properties).count
+      boo.each do |k,v|
+          boo.except!(k) unless k['slug'].present?
+      end
+      boo = Hash[boo.sort_by{|k, v| v}.reverse]
+      boo = Hash[boo.sort_by { |k,v| -v }[0..9]]
+      boo.each do |k,v|        
+          @top_posts.append(Comfy::Blog::Post.find_by(slug: k['slug']))
+      end
+      @top_posts
+    end
+    
+    @tags = Rails.cache.fetch('blog_tags', expires_in: 10.days) do 
+      @tags = Comfy::Cms::Category.of_type('Comfy::Blog::Post').all
+    end    
+    
     render layout: ComfyBlog.config.app_layout
   end
 
